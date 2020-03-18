1a4d20602fc0fb5e4fbd162356e8e3a509eae364
Edward <e.slavov11@gmail.com>
2020-03-19 01:55:02 +0200

    feat: gitignore and spring project init

---
+++ data-processing-pipeline/consumer/src/main/java/com/dataprocessingpipeline/consumer/DataProcessingPipelineConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.dataprocessingpipeline.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProcessingPipelineConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProcessingPipelineConsumerApplication.class, args);
+	}
+
+}
---
+++ data-processing-pipeline/consumer/src/test/java/com/dataprocessingpipeline/consumer/DataProcessingPipelineConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dataprocessingpipeline.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProcessingPipelineConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
