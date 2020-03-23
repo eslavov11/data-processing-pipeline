a4fa11ec0b60ad68c35cf88be1acb30d239d19ba
Edward <e.slavov11@gmail.com>
2020-03-24 01:42:30 +0200

    feat: docker-compose setup impl

--- data-processing-pipeline/data-access/src/main/java/com/dataprocessingpipeline/dataaccess/entity/MessageEntity.java
+++ data-processing-pipeline/data-access/src/main/java/com/dataprocessingpipeline/dataaccess/entity/MessageEntity.java
@@ -2,8 +2,10 @@ package com.dataprocessingpipeline.dataaccess.entity;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@Document(collection="messages")
 public class MessageEntity {
     @Id
     private String id;
